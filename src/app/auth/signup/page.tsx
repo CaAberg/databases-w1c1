2770de49d8d7bd27bfb935dcabe31be306550c49
Carl Åberg <caaberg99@gmail.com>
2025-09-26 22:38:31 +0200

    small changes for better visuals

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -5,10 +5,15 @@ const SignUpPage = () => {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <SignUpForm/>
-        <div> Already have an account? <Link className="text-red-600" href={"/auth/login"}>log in</Link></div>
+            <SignUpForm/>
+            <div className="m-8 text-center">
+                <div className="p-2 text-gray-700">Already have an account?</div>
+                
+                    <Link className="text-blue-600 p-2 hover:underline font-medium" href={"/auth/login"}>Sign in</Link>
+            
+            </div>
         </div>
-        )
+    )
 };
 
 
--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="w-full bg-gray-800 text-white px-8 py-4 sticky top-0 z-50">
+  <header className="w-full bg-gray-800 text-white px-8 py-4">
       <div className="flex flex-col md:flex-row items-center gap-4 md:grid md:grid-cols-3 max-w-7xl mx-auto">
         <div className="flex items-center hover:bg-gray-700 p-4 rounded-2xl justify-self-start">
           <h1 className="text-2xl font-bold">
-            <Link href="/" className="hover:text-gray-200">FISHERDB</Link>
+            <Link href="/" className="hover:text-gray-200">Reddit Clone</Link>
           </h1>
         </div>
         
